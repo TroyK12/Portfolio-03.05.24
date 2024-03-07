44d2025a72b5dccf292f7d63310030a325dd3779
TroyK12 <101234343+TroyK12@users.noreply.github.com>
2024-03-06 20:41:03 -0600

    change time of day update

--- src/app/HomeScreen/Greeting.tsx
+++ src/app/HomeScreen/Greeting.tsx
@@ -1,9 +1,22 @@
 "use client"
 import { useEffect, useRef } from "react";
 
-export default function Greeting({ children }: any) {
+const timeOfDay = () => {
+    const currentHour = new Date().getHours();
+  
+    if (currentHour >= 0 && currentHour < 12) {
+      return 'Good Morning!';
+    } else if (currentHour >= 12 && currentHour < 18) {
+      return 'Good Afternoon!';
+    } else {
+      return 'Good Evening!';
+    }
+  }
+
+export default function Greeting() {
     const ref = useRef<HTMLDivElement>(null);
-    
+    const greeting = timeOfDay()
+
     useEffect(()=>{
         const timeoutId = setTimeout(() => {
             if (ref.current) {
@@ -15,6 +28,6 @@ export default function Greeting({ children }: any) {
     }, []);
 
     return (
-        <h1 ref={ref} className="text-4xl md:text-7xl mt-36 ml-12">{ children }</h1>
+        <h1 ref={ref} className="text-4xl md:text-7xl mt-36 ml-12">{ greeting }</h1>
     )
 }
--- src/app/HomeScreen/HomeScreen.tsx
+++ src/app/HomeScreen/HomeScreen.tsx
@@ -3,28 +3,17 @@ import Header from "./Header";
 import mn from '@/assets/mn.png'
 import Greeting from "./Greeting"
 
-const timeOfDay = () => {
-    const currentHour = new Date().getHours();
-  
-    if (currentHour >= 0 && currentHour < 12) {
-      return 'Good Morning!';
-    } else if (currentHour >= 12 && currentHour < 18) {
-      return 'Good Afternoon!';
-    } else {
-      return 'Good Evening!';
-    }
-  }
+
 
 export default function HomeScreen() {
   const diffWords = ['Designer ', 'Developer ', 'Javascript ', 'Next.js ', 'TypeScript '];
-  const greeting = timeOfDay()
 
   return (
       <div>
         <div className="h-[90vh] w-screen flex flex-col justify-between snap-none">
           <div className="md:h-2/3">
               <div className="mt-24 ml-10 font-light text-lg tracking-widest">Duluth Minnesota</div>
-              <Greeting>{greeting}</Greeting>
+              <Greeting />
           </div>
           <div className="w-screen flex flex-col justify-end items-end overflow-hidden md:overflow-visible">
             <div className="md:m-auto mr-60 w-2 md:h-full flex flex-col md:justify-start">
