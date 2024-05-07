0ab4f65616c7c89928c7f83bd9914f7c0b5085ed
TroyK12 <101234343+TroyK12@users.noreply.github.com>
2024-05-07 17:34:53 -0600

    fix

--- src/app/About/About.tsx
+++ src/app/About/About.tsx
@@ -7,7 +7,7 @@ export default function About() {
         <div className='flex flex-col items-center overflow-y-scroll h-[60%] px-4'>
             <h1 className="text-xl py-3">Where It Started</h1>
             <p>
-                My adventure starts in the heart of the Middle East, serving my county as a satelite specialist in the summer of 2021. I specialised in the operation and maintence of our
+                My adventure starts in the heart of the Middle East, serving my country as a satelite specialist in the summer of 2021. I specialised in the operation and maintence of our
                 satelite equiptment and through that, I learned how to set up firewalls and ensure that we had the correct configuration to connect to our satelite.
                 This experience ignited my passion for learning the inner works of computers and software developement.
             </p>
