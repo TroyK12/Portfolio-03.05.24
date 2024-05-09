b6ef661dfa734e878a90acacebbb81ca84045baf
TroyK12 <101234343+TroyK12@users.noreply.github.com>
2024-05-09 16:53:49 -0600

    card img tweek

--- src/app/AboutMe/Card.tsx
+++ src/app/AboutMe/Card.tsx
@@ -20,16 +20,16 @@ export default function Card({ children, className, image, nightImage, aboutArea
         <div className="card-container">
             <div className={`relative card ${className}`}>
                 <div className="relative front-card w-[85vw] h-[70vh] md:w-[60vw] md:h-[60vh] lg:w-[25vw] lg:h-[550px] shadow-2xl rounded-2xl overflow-hidden">
-                    <Image src={image} alt="Duluth Image" className="absolute top-0 left-[-90px] md:left-0 lg:left-[-80px] h-full max-w-none w-auto" />
+                    <Image src={image} alt="Duluth Image" className="card-img absolute top-0 left-[-90px] md:left-0 lg:left-[-80px] h-full max-w-none w-auto" />
                     <div className="w-full h-full bg-[#7d7d7d41] flex flex-col items-center justify-center absolute p-5 font-thin text-3xl text-shadow">
                         {children}
                     </div>
                 </div>
                 <div className="back-card w-[85vw] h-[70vh] md:w-[60vw] md:h-[60vh] lg:w-[25vw] lg:h-[550px] rounded-2xl shadow-2xl absolute top-0 overflow-hidden">
                     <div className="relative w-full h-full">
-                    <Image src={nightImage} alt="Duluth Image" className="absolute top-0 left-[-90px] md:left-0 lg:left-[-90px] h-full max-w-none w-auto" />
+                    <Image src={nightImage} alt="Duluth Image" className="card-img absolute top-0 left-[-90px] md:left-0 lg:left-[-90px] h-full max-w-none w-auto" />
                         <div className="absolute py-7 px-3 w-full h-full text-center leading-7 flex flex-col justify-between bg-[#00000068]">
-                            <p className="letter-shadow leading-7 tracking-widest font-medium">{aboutArea}</p>
+                            <p className="letter-shadow leading-7 tracking-widest font-lg">{aboutArea}</p>
                             <CardBtn link={link}>{ linkName }</CardBtn>
                         </div>
                     </div>
