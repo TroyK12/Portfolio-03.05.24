8d78eae16886b9b66e7e7e0dca1c1fb19aed3a8b
TroyK12 <101234343+TroyK12@users.noreply.github.com>
2024-11-06 23:56:45 -0600

    content tweek

--- src/app/AboutMe/AboutMe.tsx
+++ src/app/AboutMe/AboutMe.tsx
@@ -10,19 +10,21 @@ import reactLogo from '@/assets/react-logo.png';
 import vscodeLogo from '@/assets/vscode-logo.png';
 import mongodbLogo from '@/assets/mongodb-logo.png';
 import nodeLogo from '@/assets/node-logo.png';
+import styles from './about.module.css';
 
 const aboutMeProf =
 	"I'm a full stack website designer and developer based in Duluth, mn as a freelancer. I love merging technical skills and visual design to create vibrant digital products. With a passion for design and solid development skills, I'm here to bring your vision to life and create a web experience that's uniquely yours.";
 const aboutMePersonal =
 	"I'm happiest when I'm creating, learning and exploring. When I'm not immersed in coding, you can find me snowboarding down the hills or hitting the trails for a run. My passion for adventure and the great outdoors fuels my creativity and determination in all my projects.";
 const title = 'who is troy?';
-const title2 = 'a curiosity-driven self taught freelance developer';
+const title2 = 'I am a self taught freelance developer';
+const title3 = 'who I am personally?';
 
 export default function AboutMe() {
 	return (
-		<section id="aboutMe" className="relative w-screen pt-14">
-			<div className="w-full flex flex-col md:flex-row">
-				<div className="w-full md:w-[40%] flex flex-col justify-center items-center gap-20">
+		<section id="aboutMe" className="w-screen pt-14">
+			<div className="relative w-full md:min-h-[150vh] flex flex-col md:flex-row">
+				<div className="w-full md:h-[80vh] md:w-[40%] md:sticky top-28 flex flex-col max-md:justify-center items-center gap-20">
 					<div className="pl-3">
 						<FramerTextStyles
 							duration={0.8}
@@ -32,25 +34,36 @@ export default function AboutMe() {
 					</div>
 					<FramerCardStyles />
 				</div>
-				<div className="w-full md:w-[60%] md:pr-36 pt-28 px-3 sm:px-0 flex flex-col justify-center items-center md:items-start gap-5">
-					<FramerTextStyles
-						duration={0.8}
-						text={title2}
-						className="text-lg sm:text-2xl pb-4"
-					/>
-					<FramerTextStyles
-						duration={0.3}
-						text={aboutMeProf}
-						className="font-mono text-sm sm:text-base leading-10"
-					/>
-					<FramerTextStyles
-						duration={0.5}
-						text={aboutMePersonal}
-						className="font-mono text-sm sm:text-base leading-10"
-					/>
+				<div className="w-full md:w-[60%] md:pr-36 pt-28 px-3 sm:px-0 md:mb-[200px] flex flex-col max-md:justify-center md:justify-around items-center md:items-start gap-5">
+					<div className="flex flex-col gap-8">
+						<FramerTextStyles
+							duration={0.8}
+							text={title2}
+							className="text-lg sm:text-2xl font-bold"
+						/>
+
+						<FramerTextStyles
+							duration={0.3}
+							text={aboutMeProf}
+							className={`font-mono text-sm sm:text-base ${styles.lineHeight}`}
+						/>
+					</div>
+
+					<div>
+						<FramerTextStyles
+							duration={0.8}
+							text={title3}
+							className="text-lg sm:text-2xl pb-10"
+						/>
+						<FramerTextStyles
+							duration={0.5}
+							text={aboutMePersonal}
+							className={`font-mono text-sm sm:text-base ${styles.lineHeight}`}
+						/>
+					</div>
 				</div>
 			</div>
-			<div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-8 gap-5 sm:gap-0 py-20 w-full">
+			<div className="relative z-10 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-8 gap-5 sm:gap-0 py-20 w-full">
 				<Image
 					src={htmlLogo}
 					alt="logo"
--- src/app/LandingScreen/Title.tsx
+++ src/app/LandingScreen/Title.tsx
@@ -1,28 +1,42 @@
 'use client';
 import styles from './landing.module.css';
 import { motion, useScroll, useTransform } from 'framer-motion';
-import bird from '@/assets/birds.png';
 import whiteBird from '@/assets/white-bird.png';
 import Image from 'next/image';
 
 export default function Title() {
-    let { scrollYProgress } = useScroll()
-    let x = useTransform(scrollYProgress, [0, 0.1], ["0%", "-100%"])
-    let xl = useTransform(scrollYProgress, [0, 0.1], ["0%", "120%"])
-    let y = useTransform(scrollYProgress, [0, 0.1], ["0%", "-100%"])
-    let yBird = useTransform(scrollYProgress, [0, 0.4], ["0%", "400%"])
+	let { scrollYProgress } = useScroll();
+	let x = useTransform(scrollYProgress, [0, 0.07], ['0%', '-100%']);
+	let xl = useTransform(scrollYProgress, [0, 0.05], ['0%', '120%']);
+	let y = useTransform(scrollYProgress, [0, 0.05], ['0%', '-100%']);
+	let yBird = useTransform(scrollYProgress, [0, 0.5], ['0%', '1000%']);
 
-    return (
-        <div className='relative sm:text-2xl md:text-5xl tracking-widest sm:leading-[70px] text-center'>
-            <div className='flex pl-6'>
-                <span className={styles.lets}>Lets&nbsp;</span> <span className={styles.create}> create</span>
-                <div className='overflow-hidden'><motion.div style={{ x: xl }} className={`pl-2 ${styles.new}`}>new</motion.div></div>
-            </div>
-            <div className='flex'>
-                <div className='overflow-hidden'><motion.div className={`pr-3 ${styles.possibilities}`} style={{ x }}>possibilities </motion.div></div>
-                <motion.div style={{ y }} className={styles.together}>together</motion.div>
-            </div>
-            <motion.div style={{ y: yBird }} className='absolute top-[-50px] right-20'><Image src={whiteBird} alt='bird' width={100} className='w-[100px]' /></motion.div>
-        </div>
-    )
+	return (
+		<div className="relative sm:text-2xl md:text-5xl tracking-widest sm:leading-[70px] text-center">
+			<div className="flex pl-6">
+				<span className={styles.lets}>Lets&nbsp;</span>{' '}
+				<span className={styles.create}> create</span>
+				<div className="overflow-hidden">
+					<motion.div style={{ x: xl }} className={`pl-2 ${styles.new}`}>
+						new
+					</motion.div>
+				</div>
+			</div>
+			<div className="flex">
+				<div className="overflow-hidden">
+					<motion.div className={`pr-3 ${styles.possibilities}`} style={{ x }}>
+						possibilities{' '}
+					</motion.div>
+				</div>
+				<motion.div style={{ y }} className={styles.together}>
+					together
+				</motion.div>
+			</div>
+			<motion.div
+				style={{ y: yBird }}
+				className="absolute top-[-50px] right-20">
+				<Image src={whiteBird} alt="bird" width={100} className="w-[100px]" />
+			</motion.div>
+		</div>
+	);
 }
